45ab61b90b95c7456f65f1222f4a411d1fe82e54
nicknovoa03 <nicknovoa03@gmail.com>
2021-12-21 18:13:45 -0600

    updated contract

--- src/Contract.js
+++ src/Contract.js
@@ -2,7 +2,7 @@ import Web3 from "web3";
 import accessPass from "./contracts/BlackBoxAccessPass.json"
 
 
-const contractAddr = "";
+const contractAddr = "0xe7ca2cD6b0247D32f6d5aE1FD09Eb62539E010C4";
 const web3 = new Web3("https://mainnet.infura.io/v3/bad8cc770bef49dc88683bf2290205c8");
 let contract = new web3.eth.Contract(accessPass.abi, contractAddr);
 
